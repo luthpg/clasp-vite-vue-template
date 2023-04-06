0b79a35497cb3375d24b0f9bb58bb5dc66cd1876
luth <luthalto@gmail.com>
2023-04-06 16:45:50 +0900

    [fix] createHtmlOutputFromFile

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,6 @@
 function doGet(e: WebAppParams) {
   const title = 'React + GAS';
-  return HtmlService.createHtmlOutput('hosting/index')
+  return HtmlService.createHtmlOutputFromFile('hosting/index')
     .addMetaTag('viewport', 'width=device-width, initial-scale=1')
     .setTitle(title);
 }
